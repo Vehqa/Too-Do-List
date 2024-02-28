19b57a414d98aa73f3273d5285f27db56563903f
Vehqa <pro.nectoux@gmail.com>
2024-02-28 13:37:17 +0100

    UI / Extract widgets.

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,12 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/widgets/tasks_list.dart';
 
 class TasksScreen extends StatelessWidget {
   const TasksScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.blueGrey,
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add a new taks !
+        },
+        backgroundColor: Colors.redAccent,
+        child: const Icon(
+          Icons.add,
+          size: 30,
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+              top: 60,
+              left: 30,
+              right: 30,
+              bottom: 30,
+            ), // Exemple de padding avec une marge de 16 pixels de chaque côté
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.list,
+                    size: 30,
+                    color: Colors.redAccent,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'Tasker',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const Text(
+                  '11 Tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: TasksList(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
